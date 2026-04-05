0a808e9ccb7f7b97d9220e1c21ebf25c423bd3a8
Mauro Aviles Santos <chamberviolin420@gmail.com>
2026-04-05 17:44:25 -0500

    Added srand for true randomness instead of a set seed

--- Pokeball_Catch._Mechanic.cpp
+++ Pokeball_Catch._Mechanic.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -9,17 +10,18 @@ int main(){
     int catch_attempt = 0;
 
     while(true){
+        srand(time(0)); // seed for true randomness
         cout << "Start Catching? (y/n): ", cin >> input;
         if(input == 'y'){
             catch_attempt = rand() % 100 + 1; // rand num between 1 and 100
             if(catch_attempt <= 10){
                 cout << "Four wiggles.... Pokemon Caught!\n";
                 break;
-            } else if(catch_attempt <= 15){
+            } else if(catch_attempt <= 20){
                 cout << "Three wiggles... Escaped!\n";
-            } else if(catch_attempt <= 30){
+            } else if(catch_attempt <= 40){
                 cout << "Two wiggles.. Escaped!\n";
-            } else if(catch_attempt <= 75){
+            } else if(catch_attempt <= 80){
                 cout << "One wiggle. Escaped!\n";
             } else {
                 cout << "No wiggles it escaped!\n";
